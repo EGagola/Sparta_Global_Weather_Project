5543fb9011db8f9695f1569f19a0aa41bc8b0fe3
Elliot Gagola <egagola@spartaglobal.com>
2018-11-28 19:20:13 +0000

    V.0.1.0 General methods working for starter tests

--- lib/services/five_day_weather_service.rb
+++ lib/services/five_day_weather_service.rb
@@ -8,15 +8,36 @@ class FiveDayService
   def initialize
     @longitude = 20
     @latitude = 30
+    @five_day_forecast = 0
   end
 
 
   def get_five_day_data(api_key)
-    @five_day_service = JSON.parse(self.class.get("http://api.openweathermap.org/data/2.5/forecast?lat=#{@latitude}&lon=#{@longitude}&APPID=#{api_key}").body)
+    @five_day_forecast = JSON.parse(self.class.get("http://api.openweathermap.org/data/2.5/forecast?lat=#{@latitude}&lon=#{@longitude}&APPID=#{api_key}").body)
   end
 
   def get_list
-    @five_day_service['list']
-    
+    @five_day_forecast['list']
   end
+
+  def search_outer(search_word)
+    @five_day_forecast["#{search_word}"]
+  end
+
+  def search_inside_city_for(search_word)
+  @five_day_forecast['city']["#{search_word}"]
+end
+
+def search_list_for(iterator, search_word)
+  @five_day_forecast['list'][iterator]["#{search_word}"]
+end
+
+def search_weather_for(iterator, search_word)
+  @five_day_forecast['list'][iterator]['weather'][0]["#{search_word}"]
+end
+
+def search_list_input_for(iterator, input, search_word)
+  @five_day_forecast['list'][iterator]["#{input}"]["#{search_word}"]
+end
+
 end
--- spec/five_day_weather_spec.rb
+++ spec/five_day_weather_spec.rb
@@ -5,10 +5,26 @@ describe FiveDayService do
   before(:all) do
     @api_key = ENV['OPENWEATHERMAP_API_KEY']
     @five_day_service = OpenWeather.new.five_day_service
-    @five_day_forecast = @five_day_service.get_five_day_data(@api_key)
+    @five_day_service.get_five_day_data(@api_key)
   end
 
   it "should be a hash" do
-    expect(@five_day_forecast).to be_kind_of Hash
+    expect(@five_day_service.get_five_day_data(@api_key)).to be_kind_of Hash
+  end
+
+  it "should have a length of 5" do
+    expect(@five_day_service.get_five_day_data(@api_key).length).to eq 5
+  end
+
+  it "should have a cod message of 200" do
+    expect(@five_day_service.search_outer('cod').to_i).to eq 200
+  end
+
+  it "should have a message that is a float" do
+    expect(@five_day_service.search_outer('message')).to be_kind_of Float
+  end
+
+  it "should have a list length that is equal to the count set" do
+    expect(@five_day_service.search_outer('list').length).to eq(@five_day_service.search_outer('cnt'))
   end
 end
