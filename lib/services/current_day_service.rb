7e74e473e088635b034aee35f93c0dedd5d97c1f
halil-bolat <hbolat@spartaglobal.com>
2018-11-28 17:15:28 +0000

    Using sample stub as data, getting right information ready to move on to creating tests and methods!

--- lib/services/current_day_service.rb
+++ lib/services/current_day_service.rb
@@ -1,9 +1,22 @@
 require 'httparty'
 require 'json'
+require_relative '../generators/id_generator'
 
 class CurrentDayService
   include HTTParty
 
-  base_uri ''
+  base_uri 'https://samples.openweathermap.org'
+
+  def initialize
+    @current_weather = CityIdGenerator.new
+    @current_weather = @current_weather.weather
+  end
+
+  def test
+    @current_weather
+  end
 
 end
+
+new = CurrentDayService.new
+puts new.test
