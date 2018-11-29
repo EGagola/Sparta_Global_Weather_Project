49eee40c1fe1c18204571538edc7fc7496302a12
halil-bolat <hbolat@spartaglobal.com>
2018-11-29 22:18:00 +0000

    debugging

--- spec/single_current_weather_spec.rb
+++ spec/single_current_weather_spec.rb
@@ -5,7 +5,7 @@ describe OpenWeather do
   attr_accessor :api_key
 
   before(:all) do
-    @api_key = 
+    @api_key = 'a210fbef76875d625711834647e06f34'
     @single_weather_service = OpenWeather.new.single_weather_service
     @single_weather_service.get_single_weather_id(@api_key)
     @current_day = CurrentDayService.new
