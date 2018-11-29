c99c289ab87e1a8669df100b6de0c83e3a506a5e
Elliot Gagola <egagola@spartaglobal.com>
2018-11-29 14:47:56 +0000

    V.0.1.0 Parsed data out of JSON ready to attempt testing

---
+++ lib/generators/coordinates_generator.rb
@@ -0,0 +1,23 @@
+require 'json'
+
+class ParseJSON
+
+  attr_accessor :json_file
+
+  def initialize json_file
+    @json_file = JSON.parse(File.read(json_file))
+  end
+
+  def get_input number , input
+    puts @json_file[number]["#{input}"]
+  end
+
+  def get_coord number , direction
+    @json_file[number]['coord']["#{direction}"]
+  end
+
+end
+# 
+# newt = ParseJSON.new('../../city.list.json')
+# puts newt.get_coord(100,'lon')
+# puts newt.get_coord(100,'lat')
