58e08903c645cd88e3b091111bbfdfb87c517f1d
Oliver Sharpe <osharpe@spartaglobal.com>
2018-11-29 11:52:03 +0000

    V.1.3.0 implemented the generators for coordinates

--- lib/services/current_day_coordinate_service.rb
+++ lib/services/current_day_coordinate_service.rb
@@ -1,9 +1,33 @@
 require 'httparty'
 require 'json'
+require_relative '../generators/coordinates_generator'
 
-class SixteenDayService
+class CurrentDayCoordinateService
   include HTTParty
 
-  base_uri ''
+  def initialize
+    # placeholders for the generator functions
+    @coordinates = CoordsGenerator.new
+    @latitude = @coordinates.generate_latitude
+    @longitude = @coordinates.generate_longitude
+    @current_day = {}
+  end
+
+  def get_current_day_data(api_key)
+    # api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
+    @current_day = JSON.parse(self.class.get("http://api.openweathermap.org/data/2.5/weather?lat=#{@latitude}&lon=#{@longitude}&APPID=#{api_key}").body)
+  end
+
+  def search_outer(search_word)
+    @current_day["#{search_word}"]
+  end
+
+  def search_weather_for(search_word)
+    @current_day['weather'][0]["#{search_word}"]
+  end
+
+  def search_input_for(input, search_word)
+    @current_day["#{input}"]["#{search_word}"]
+  end
 
 end
