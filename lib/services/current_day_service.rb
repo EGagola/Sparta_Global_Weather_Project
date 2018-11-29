8bb19e6dcbbaca1f4e438da03e9350b048e06aa7
Sana Rafaqat <sanarafaqat041@gmail.com>
2018-11-29 10:02:47 +0000

    V.0.0.3 Took out the service current weather

--- lib/services/current_day_service.rb
+++ lib/services/current_day_service.rb
@@ -6,12 +6,4 @@ class CurrentDayService
 
   base_uri 'https://samples.openweathermap.org'
 
-  def initialize
-    @current_weather = CityIdGenerator.new
-    @current_weather = @current_weather.weather
-  end
-
-  def test
-    @current_weather
-  end
 end
